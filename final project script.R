0debf4e634a4936a949af1ef496e5977f96424a7
moffab <bmoffa@colgate.edu>
2020-10-30 12:28:50 -0400

    data formatting done

--- final project script.R
+++ final project script.R
@@ -1,4 +1,7 @@
 
+install.packages("usmap")
+library(usmap)
+
 #import air quality data and change names of population and cbsacode columns
 airDatO<-read.csv("/Users/benjaminmoffa/Documents/Github/GEOG331project/airdata1.csv", skip=2)
 airDat<-subset(airDatO, airDatO$CBSA.Code>0)
@@ -18,5 +21,26 @@ mergeddata<-merge(airDat, crossDat, by="cbsacode")
 mDat<-subset(mergeddata, mergeddata$metropolitanmicropolitanstatis == "Metropolitan Statistical Area")
 #rename column 19 as county
 names(mDat)[19]<-"county"
+mDat1<-subset(mDat, mDat$statename != "Puerto Rico")
 
-cDat<-merge(mDat, covidDat, by="county")
+#create new fips code for mDat
+for (n in 1:691){
+  tryCatch({
+mDat1$countynew[n]<-fips(mDat1$statename[n], county = mDat1$county[n])
+  }, error=function(e){})
+}
+
+#create new fips code for covidDat
+for (n in 1:3244){
+  tryCatch({
+    covidDat$fips1[n]<-fips(covidDat$state[n], county = covidDat$county[n])
+  }, error=function(e){})
+}
+
+#rename columns for merge
+names(covidDat)[4]<-"fips1"
+names(mDat1)[24]<-"fips1"
+
+#merge covid data and metro/air data
+cDat<-merge(mDat1, covidDat, by="fips1")
+
